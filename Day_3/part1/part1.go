538d6358dac225a09edb8990a9adef4454277d67
myshkovsky <84045642+myshkovsky@users.noreply.github.com>
2024-01-30 05:47:22 +0100

    Update Day_3/part1

--- Day_3/part1/part1.go
+++ Day_3/part1/part1.go
@@ -8,6 +8,40 @@ import (
 	"strconv"
 )
 
+func CheckUpDown(line int, start int, end int, up bool, lines []string, re *regexp.Regexp, maxLineLength int) bool {
+	var direction int
+	if up {
+		direction = -1
+	} else {
+		direction = 1
+	}
+	var r []int
+	if start == 0 {
+		r = re.FindStringIndex(lines[line+direction][start : end+1])
+	} else if end == maxLineLength {
+		r = re.FindStringIndex(lines[line+direction][start-1 : end])
+	} else {
+		r = re.FindStringIndex(lines[line+direction][start-1 : end+1])
+	}
+	return r != nil
+}
+
+func CheckSides(line int, start int, end int, lines []string, re *regexp.Regexp, maxLineLength int) bool {
+	isLeftSymbol := func() bool {
+		return string(lines[line][start-1]) != "."
+	}
+	isRightSymbol := func() bool {
+		return string(lines[line][end]) != "."
+	}
+	if start == 0 {
+		return isRightSymbol()
+	} else if end == maxLineLength {
+		return isLeftSymbol()
+	} else {
+		return isRightSymbol() || isLeftSymbol()
+	}
+}
+
 func Solve(file *os.File) {
 	sumParts := 0
 	s := bufio.NewScanner(file)
@@ -24,9 +58,6 @@ func Solve(file *os.File) {
 		currentLine++
 	}
 
-	numLines := len(lines)
-	maxLineLength := len(lines[0])
-
 	addPart := func(str string) {
 		num, err := strconv.Atoi(str)
 		if err != nil {
@@ -35,57 +66,28 @@ func Solve(file *os.File) {
 		sumParts += num
 	}
 
+	numLines := len(lines)
+	maxLineLength := len(lines[0])
 	re := regexp.MustCompile(`[^.0-9]+`)
-	checkUpDown := func(line int, start int, end int, up bool) bool {
-		var direction int
-		if up {
-			direction = -1
-		} else {
-			direction = 1
-		}
-		var r []int
-		if start == 0 {
-			r = re.FindStringIndex(lines[line+direction][start : end+1])
-		} else if end == maxLineLength {
-			r = re.FindStringIndex(lines[line+direction][start-1 : end])
-		} else {
-			r = re.FindStringIndex(lines[line+direction][start-1 : end+1])
-		}
-		return r != nil
-	}
-
-	checkSides := func(line int, start int, end int) bool {
-		isLeftSymbol := func() bool {
-			return string(lines[line][start-1]) != "."
-		}
-		isRightSymbol := func() bool {
-			return string(lines[line][end]) != "."
-		}
-		if start == 0 {
-			return isRightSymbol()
-		} else if end == maxLineLength {
-			return isLeftSymbol()
-		} else {
-			return isRightSymbol() || isLeftSymbol()
-		}
-	}
 
 	for k, v := range nMap {
 		for _, y := range v {
 			if k == 0 {
-				if checkSides(k, y[0], y[1]) || checkUpDown(k, y[0], y[1], false) {
+				if CheckSides(k, y[0], y[1], lines, re, maxLineLength) ||
+					CheckUpDown(k, y[0], y[1], false, lines, re, maxLineLength) {
 					addPart(lines[k][y[0]:y[1]])
 					continue
 				}
 			} else if k == numLines-1 {
-				if checkUpDown(k, y[0], y[1], true) || checkSides(k, y[0], y[1]) {
+				if CheckUpDown(k, y[0], y[1], true, lines, re, maxLineLength) ||
+					CheckSides(k, y[0], y[1], lines, re, maxLineLength) {
 					addPart(lines[k][y[0]:y[1]])
 					continue
 				}
 			} else {
-				if checkSides(k, y[0], y[1]) ||
-					checkUpDown(k, y[0], y[1], true) ||
-					checkUpDown(k, y[0], y[1], false) {
+				if CheckSides(k, y[0], y[1], lines, re, maxLineLength) ||
+					CheckUpDown(k, y[0], y[1], true, lines, re, maxLineLength) ||
+					CheckUpDown(k, y[0], y[1], false, lines, re, maxLineLength) {
 					addPart(lines[k][y[0]:y[1]])
 					continue
 				}
